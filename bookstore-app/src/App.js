ff2eecd2bffab087ffa873fa563ce366bf6cd43b
oluwadydx <arogundadeadedoyin@gmail.com>
2025-12-14 22:52:38 +0100

    Your commit message

---
+++ bookstore-app/src/App.js
@@ -0,0 +1,30 @@
+// import { Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Home from './pages/Home';
+// import BookList from './pages/BookList';
+// import BookDetails from './pages/BookDetails';
+// import Cart from './pages/Cart';
+// import Checkout from './pages/Checkout';
+// import OrderConfirmation from './pages/OrderConfirmation';
+
+// function App() {
+//   return (
+//     <div className="min-h-screen flex flex-col">
+//       <Header />
+//       <main className="flex-grow">
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/books" element={<BookList />} />
+//           <Route path="/books/:id" element={<BookDetails />} />
+//           <Route path="/cart" element={<Cart />} />
+//           <Route path="/checkout" element={<Checkout />} />
+//           <Route path="/order-confirmation" element={<OrderConfirmation />} />
+//         </Routes>
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
